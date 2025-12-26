43a0ed737b3f59c69bcdfd3b9f5ff0dc54dc0bcf
Niku2009sha <nikusha.chigladze2009@gmail.com>
2025-12-26 13:35:38 +0400

    rooms-done

--- src/app/main pages/home/home.ts
+++ src/app/main pages/home/home.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Service } from '../../services/service';
+import { pipe, takeUntil, tap, catchError, of, finalize, Subject } from 'rxjs';
+import { roomCard } from '../../models/model.interface';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +9,33 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit, OnDestroy {
+  public service = inject(Service);
+  public roomsData: roomCard[] | undefined;
+  public hasError: boolean = false;
+  public destroy$ = new Subject();
 
+  ngOnInit() {
+    this.service
+      .roomsAll()
+      .pipe(
+        takeUntil(this.destroy$),
+        tap((data) => {
+          this.roomsData = data as unknown as roomCard[];
+        }),
+        catchError(() => {
+          this.hasError = true;
+          return of('error');
+        }),
+        finalize(() => {
+          console.log('Rooms data loaded');
+        })
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next; 
+    this.destroy$.complete();
+  }
 }
--- src/app/services/service.ts
+++ src/app/services/service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Rooms } from '../main pages/rooms/rooms';
+import { Home } from '../main pages/home/home';
+import { roomCard } from '../models/model.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +11,15 @@ import { Rooms } from '../main pages/rooms/rooms';
 export class Service {
   public http = inject(HttpClient)
 
-  public roomsAll():Observable<Rooms> {
-    return this.http.get<Rooms>(
+  public roomsAll(): Observable<roomCard[]> {
+    return this.http.get<roomCard[]>(
       "https://hotelbooking.stepprojects.ge/api/Rooms/GetAll"
+    )
+  }
+
+  public getRoomById(id: number): Observable<roomCard> {
+    return this.http.get<roomCard>(
+      `https://hotelbooking.stepprojects.ge/api/Rooms/GetRoom/${id}`
     )
   }
 }
